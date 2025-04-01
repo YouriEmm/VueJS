01b3ac9a7226004413c186c16b17359b47f31c4d
YouriEmmanuel <youri.emmanuel@iut-dijon.u-bourgogne.fr>
2025-04-01 11:36:33 +0200

    Ajout template avec tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import htmlContent from '../template-html/index.html?raw'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main v-html="htmlContent"></main>
+  <!-- <TheWelcome /> -->
 </template>
