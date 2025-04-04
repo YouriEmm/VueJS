52e064afe2751ddf00d22c6c365015ca5d64ec75
YouriEmmanuel <youri.emmanuel@iut-dijon.u-bourgogne.fr>
2025-04-04 16:20:50 +0200

    Modification panier (séparation composant)

---
+++ src/components/productCheckout.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  product: Object,
+})
+
+const emit = defineEmits(['remove'])
+
+const removeItem = () => {
+  emit('remove', props.product.id)
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-between border-b pb-4">
+    <div class="flex items-center gap-4">
+      <img :src="product.image" class="w-20 h-20 object-contain rounded" alt="Produit" />
+      <div>
+        <p class="font-medium">{{ product.name }}</p>
+        <p class="text-gray-500 text-sm">Quantité : {{ product.quantity }}</p>
+      </div>
+    </div>
+    <div class="text-right">
+      <p class="font-bold">{{ (product.price * product.quantity).toFixed(2) }} €</p>
+      <button @click="removeItem" class="text-red-500 text-sm hover:underline mt-1">
+        Supprimer
+      </button>
+    </div>
+  </div>
+</template>
--- src/views/Panier.vue
+++ src/views/Panier.vue
@@ -1,23 +1,51 @@
 <script setup>
 import { computed, ref } from 'vue'
+import CartItem from '../components/productCheckout.vue'
 
 const productsInCheckout = ref([
-  { id: 1, name: 'Basket Blanche', quantity: 2, price: 28.99 },
-  { id: 2, name: 'Basket Noire', quantity: 2, price: 18.99 },
-  { id: 3, name: 'Basket Rose', quantity: 2, price: 24.99 },
-  { id: 4, name: 'Basket Rouge', quantity: 2, price: 30.99 },
+  {
+    id: 1,
+    name: 'Nike Air Force 1 Low 07',
+    quantity: 2,
+    price: 78,
+    image:
+      'https://images.stockx.com/360/Nike-Air-Force-1-Low-White-07/Images/Nike-Air-Force-1-Low-White-07/Lv2/img13.jpg?w=576&q=60&dpr=1&updated_at=1635275427&h=384',
+  },
+  {
+    id: 2,
+    name: 'Jordan 12 rétro',
+    quantity: 2,
+    price: 244.99,
+    image:
+      'https://images.stockx.com/360/Air-Jordan-12-Retro-Flu-Game-2025/Images/Air-Jordan-12-Retro-Flu-Game-2025/Lv2/img13.jpg?w=576&q=60&dpr=1&updated_at=1740507702&h=384',
+  },
+  {
+    id: 3,
+    name: 'Addidas D Rose 10',
+    quantity: 4,
+    price: 266,
+    image:
+      'https://images.stockx.com/images/adidas-D-Rose-10-Metallic-Gold.jpg?fit=fill&bg=FFFFFF&w=576&h=384&q=60&dpr=1&trim=color&updated_at=1644449664',
+  },
+  {
+    id: 4,
+    name: 'Jordan 1 Retro High OG SP',
+    quantity: 2,
+    price: 226.99,
+    image:
+      'https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-SP-Union-LA-Chicago-Shadow/Images/Air-Jordan-1-Retro-High-OG-SP-Union-LA-Chicago-Shadow/Lv2/img01.jpg?w=576&q=60&dpr=1&updated_at=1740687534&h=384',
+  },
 ])
 
 const totalPrice = computed(() => {
-  return productsInCheckout.value.reduce((acc, product) => {
-    return acc + product.price * product.quantity
-  }, 0)
-  return result
+  return productsInCheckout.value.reduce(
+    (acc, product) => acc + product.price * product.quantity,
+    0,
+  )
 })
 
 const removeProduct = (id) => {
   productsInCheckout.value = productsInCheckout.value.filter((product) => product.id !== id)
-  console.log('produit supprimé')
 }
 </script>
 
@@ -26,43 +54,21 @@ const removeProduct = (id) => {
     <h2 class="text-2xl font-bold mb-6">Mon Panier</h2>
 
     <div class="space-y-6">
-      <div
+      <CartItem
         v-for="product in productsInCheckout"
         :key="product.id"
-        class="flex items-center justify-between border-b pb-4"
-      >
-        <div class="flex items-center gap-4">
-          <img
-            src="https://via.placeholder.com/100"
-            class="w-20 h-20 object-cover rounded"
-            alt="Produit"
-          />
-          <div>
-            <p class="font-medium">{{ product.name }}</p>
-            <p class="text-gray-500 text-sm">Quantité : {{ product.quantity }}</p>
-          </div>
-        </div>
-        <div class="text-right">
-          <p class="font-bold">{{ product.price * product.quantity }} €</p>
-          <button
-            @click="removeProduct(product.id)"
-            class="text-red-500 text-sm hover:underline mt-1"
-          >
-            Supprimer
-          </button>
-        </div>
-      </div>
+        :product="product"
+        @remove="removeProduct"
+      />
+    </div>
 
-      <div class="text-right mt-10">
-        <p class="text-lg font-semibold">
-          Total : <span class="text-blue-600">{{ totalPrice }} €</span>
-        </p>
-        <button
-          class="mt-4 bg-blue-600 text-white px-6 py-3 rounded-xl hover:bg-blue-700 transition"
-        >
-          Commander
-        </button>
-      </div>
+    <div class="text-right mt-10">
+      <p class="text-lg font-semibold">
+        Total : <span class="text-blue-600">{{ totalPrice }} €</span>
+      </p>
+      <button class="mt-4 bg-blue-600 text-white px-6 py-3 rounded-xl hover:bg-blue-700 transition">
+        Commander
+      </button>
     </div>
   </main>
 </template>
